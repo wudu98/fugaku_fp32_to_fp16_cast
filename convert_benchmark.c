ebbdee2718c5eb5080b6fe96695e5ec3c01f4fa9
wudu98 <1150269136@qq.com>
2023-09-06 14:46:24 +0800

    wudu

--- convert_benchmark.c
+++ convert_benchmark.c
@@ -58,7 +58,7 @@ void fp32_stream_copy(int M, int N, int lda, int n_loops) {
   free(A_out);
 }
 
-void fp32_convert_fp16_copy(int M, int N, int lda, int n_loops) {
+void fp32_convert_fp16_copy_v0(int M, int N, int lda, int n_loops) {
   float  *A_in  = malloc(M * lda * sizeof(float));
   __fp16 *A_out = malloc(M * lda * sizeof(__fp16));
   init(A_in, M * lda);
@@ -94,6 +94,63 @@ void fp32_convert_fp16_copy(int M, int N, int lda, int n_loops) {
   free(A_out);
 }
 
+
+void fp32_convert_fp16_copy_v1(int M, int N, int lda, int n_loops) {
+  const int mc = 4;
+  const int nc = 128;
+
+  float  *A_in  = malloc(M * lda * sizeof(float));
+  __fp16 *A_out = malloc(M * lda * sizeof(__fp16));
+  init(A_in, M * lda);
+  memset(A_out, 0.0, M * lda * sizeof(__fp16));
+
+  struct timespec start, end;
+  double time_used = 0.0;
+  clock_gettime(CLOCK_MONOTONIC_RAW, &start);
+  for (int _loop = 0; _loop < n_loops; ++_loop) {
+    #pragma omp parallel for collapse(2)
+    for (int i = 0; i < M; i+=mc){
+      for (int j = 0; j < N; j+=nc){
+        asm volatile(
+          "ptrue  p0.s                                           \n"
+          "add    x6, %[A_in], %[offset_m], lsr #9               \n"
+          "add    x6, x6, %[offset_n], lsr #2                    \n"
+          "add    x7, %[A_out], %[offset_m], lsr #8              \n"
+          "add    x7, x7, %[offset_n], lsr #1                    \n"
+          "ld1w   z0.s, p0/z, [x6]                               \n"
+          "fcvt   z1.h, p0/m, z0.s                               \n"
+          "st1w   z1.h, p0, [x7]                                 \n"
+
+          : [A_out]"=r"(A_out)
+          : "0"(A_out),
+            [A_in]"r"(A_in),
+            [offset_m]"r"(i),
+            [offset_n]"r"(j)
+          : "cc", "memory" , "x6", "x7", "z0", "z1"
+        );
+        // A_out[i * lda + j] = (__fp16)A_in[i * lda + j];
+      }
+    }
+  }
+  clock_gettime(CLOCK_MONOTONIC_RAW, &end);
+  time_used = get_time(&start, &end);
+  printf("fp32 convert fp16 latency: %.6f ms\n", time_used * 1e3 / n_loops);
+  printf("A_in[0] = %.6f, A_out[0] = %.6f\n", A_in[0], A_out[0]);
+  printf("A_in[0] = %#x, A_out[0] = %#x\n", ((int*)A_in)[0], ((short*)A_out)[0]);
+  int tmp = *(int*)(&A_in[0]);
+  short t = ((tmp & 0x007fffff) >> 13) | ((tmp & 0x80000000) >> 16) | (((tmp & 0x7f800000) >> 13) - ((127 - 15) << 10));
+  if (tmp & 0x1000) {t++;}
+  printf("double check A_in[0] convert to fp16 = %#x\n", t);
+  printf("A_in[1] = %.6f, A_out[1] = %.6f\n", A_in[1], A_out[1]);
+  printf("A_in[1] = %#x, A_out[1] = %#x\n", ((int*)A_in)[1], ((short*)A_out)[1]);
+  tmp = *(int*)(&A_in[1]);
+  t = ((tmp & 0x007fffff) >> 13) | ((tmp & 0x80000000) >> 16) | (((tmp & 0x7f800000) >> 13) - ((127 - 15) << 10));
+  if (tmp & 0x1000) {t++;}
+  printf("double check A_in[1] convert to fp16 = %#x\n", t);
+  free(A_in);
+  free(A_out);
+}
+
 int main(){
     int num_threads = 48;
     omp_set_num_threads(num_threads);
@@ -107,6 +164,9 @@ int main(){
     fp32_stream_copy(M, K, lda, 10);
     printf("-----------------------\n");
 
-    fp32_convert_fp16_copy(M, K, lda, 10);
+    fp32_convert_fp16_copy_v0(M, K, lda, 10);
+    printf("-----------------------\n");
+
+    fp32_convert_fp16_copy_v1(M, K, lda, 10);
     printf("-----------------------\n");
 }
