0d03c5205670693e4c9c7d5986b79881810a2cbd
wudu98 <1150269136@qq.com>
2023-09-07 17:55:46 +0800

    wudu

--- convert_benchmark.c
+++ convert_benchmark.c
@@ -320,7 +320,7 @@ void fp32_convert_fp16_copy_v3(int M, int N, int lda, int n_loops) {
         t = ((tmp & 0x007fffff) >> 13) | ((tmp & 0x80000000) >> 16) | (((tmp & 0x7f800000) >> 13) - ((127 - 15) << 10));
         if (tmp & 0x1000) {t++;}
         if ( fabs(A_out[offset] - (*(__fp16*)(&t))) > 6e-3 && flag == 1){
-          printf("error [%d][%d]\n", i, j);
+          printf("error [%d][%d], %#x, %#x\n", i, j, ((short*)A_out)[offset], t);
           flag = 0;
         }
         // if ( *(short*)(&A_out[offset]) != t && flag == 1){
