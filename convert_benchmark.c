8b5208bb47147d939c846dfc794f751f2c803d3a
wudu98 <1150269136@qq.com>
2023-09-07 18:08:39 +0800

    wudu

--- convert_benchmark.c
+++ convert_benchmark.c
@@ -184,7 +184,8 @@ void fp32_convert_fp16_copy_v2(int M, int N, int lda, int n_loops) {
     #pragma omp parallel for collapse(2)
     for (int i = 0; i < M; i++){
       for (int j = 0; j < N; j+=128){
-        for (int jj = 0; jj < 128; jj++){
+        int _up = max(128, N - j);
+        for (int jj = 0; jj < _up; jj++){
           A_out[i * lda + j + jj] = (__fp16)A_in[i * lda + j + jj];
         }
       }
